9634af7cacee93bd82ba44733039ba4c00e4f1bc
Colton Morley <ColtonMorley42@gmail.com>
2024-08-06 17:37:08 -0700

    Add content for Locations, menu, catering

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './style.css'
 
 console.log("Ready bromine")
+
+
+/*
+picture usage
+
+import Icon from './icon.png';
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+*/ 
+
+
